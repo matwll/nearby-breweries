2fb1c904bc19391b3f089bbc55307cbf07a95574
“Spencer <srye1140@gmail.com>
2022-11-03 19:35:33 -0700

    Adjusts code to work with search button and search parameters

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,20 +1,25 @@
+//this is assuming .breweryList is 
 var breweryListEl = document.querySelector(".breweryList");
-var searchButtonEl = document.querySelector(".searchButton");
-var userInputEl = document.querySelector(".userInput")
+var searchBtnEl = document.querySelector("#searchBtn");
+var inputFieldEl = document.querySelector(".input-field")
 
 function getApi() {
-  var requestURL = 'https://api.giphy.com/v1/gifs/random?api_key=JPHvBSAvBJm1NQqutblYluzvAYw7dE6O&tag=' + userInputEl.innerHTML + '&rating=pg-13';
-
+  var requestURL = 'https://api.giphy.com/v1/gifs/random?api_key=JPHvBSAvBJm1NQqutblYluzvAYw7dE6O&tag=' + inputFieldEl.value + '&rating=pg-13';
+  console.log(inputFieldEl.value);
+  console.log(requestURL);
   fetch(requestURL)
     .then(function(response) {
       return response.json();
     })
     .then(function (jsonArray) {
       console.log(jsonArray);
-      breweryListEl.src = jsonArray.data.images.fixed_height.mp4
+      var randGif = document.createElement("img");
+      randGif.src = jsonArray.data.images.fixed_height.url;
+      breweryListEl.appendChild(randGif);
     })
 };
 
 
-//searchButtonEl.addEventListener("click", getapi());
-getApi()
+searchBtnEl.addEventListener("click", function() {
+  getApi()
+});
